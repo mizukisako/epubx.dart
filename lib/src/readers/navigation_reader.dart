598f2412aa427a3fadc5c25d0f62428705acba86
mizuki sako <mizuki_sako@ai-j.jp>
2024-03-11 17:21:23 +0900

    deprecated property対応

--- lib/src/readers/navigation_reader.dart
+++ lib/src/readers/navigation_reader.dart
@@ -240,7 +240,7 @@ class NavigationReader {
     result.Authors = <String>[];
     docAuthorNode.children.whereType<xml.XmlElement>().forEach((xml.XmlElement textNode) {
       if (textNode.name.local.toLowerCase() == 'text') {
-        result.Authors!.add(textNode.text);
+        result.Authors!.add(textNode.innerText);
       }
     });
     return result;
@@ -251,7 +251,7 @@ class NavigationReader {
     result.Titles = <String>[];
     docTitleNode.children.whereType<xml.XmlElement>().forEach((xml.XmlElement textNode) {
       if (textNode.name.local.toLowerCase() == 'text') {
-        result.Titles!.add(textNode.text);
+        result.Titles!.add(textNode.innerText);
       }
     });
     return result;
@@ -302,14 +302,14 @@ class NavigationReader {
       throw Exception('Incorrect EPUB navigation label: label text element is missing.');
     }
 
-    result.Text = navigationLabelTextNode.text;
+    result.Text = navigationLabelTextNode.innerText;
 
     return result;
   }
 
   static EpubNavigationLabel readNavigationLabelV3(xml.XmlElement navigationLabelNode) {
     var result = EpubNavigationLabel();
-    result.Text = navigationLabelNode.text.trim();
+    result.Text = navigationLabelNode.innerText.trim();
     return result;
   }
 
